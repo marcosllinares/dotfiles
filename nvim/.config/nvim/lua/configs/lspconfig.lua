10d2ae40609f43f5e216a974f63dd4aaf479419c
Marcos Llinares <alu0100972443@ull.edu.es>
2025-09-22 21:03:39 +0100

    Cambios en lspconfig

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -6,7 +6,7 @@ vim.lsp.enable(servers)
 -- ========== Diagnostics base ==========
 vim.diagnostic.config({
   vitual_lines = true,
-  virtual_text = { prefix = "●", spacing = 2 },   -- texto inline
+  -- virtual_text = { prefix = "●", spacing = 2 },   -- texto inline
   signs = true,                                    -- signos en el gutter
   underline = true,
   update_in_insert = false,
@@ -33,5 +33,61 @@ vim.diagnostic.config({
 --   local hl = "DiagnosticSign" .. type
 --   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 -- end
+
+
+-- ===============================
+-- Keymaps + highlight al adjuntar LSP
+-- ===============================
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
+  callback = function(event)
+    local map = function(keys, func, desc)
+      vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+    end
+
+    -- defaults
+    map("gl", vim.diagnostic.open_float, "Open Diagnostic Float")
+    map("K", vim.lsp.buf.hover, "Hover Documentation")
+    map("gs", vim.lsp.buf.signature_help, "Signature Documentation")
+    map("gD", vim.lsp.buf.declaration, "Goto Declaration")
+    map("<leader>la", vim.lsp.buf.code_action, "Code Action")
+    map("<leader>lr", vim.lsp.buf.rename, "Rename all references")
+    map("<leader>lf", vim.lsp.buf.format, "Format")
+    map("<leader>V", "<cmd>vsplit | lua vim.lsp.buf.definition()<cr>", "Goto Definition in Vertical Split")
+
+    -- highlight símbolos bajo cursor
+    local function client_supports_method(client, method, bufnr)
+      if vim.fn.has "nvim-0.11" == 1 then
+        return client.supports_method(method, { bufnr = bufnr })
+      else
+        return client.supports_method(method)
+      end
+    end
+
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+      local highlight_augroup = vim.api.nvim_create_augroup("lsp-highlight", { clear = false })
+
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd("LspDetach", {
+        group = vim.api.nvim_create_augroup("lsp-detach", { clear = true }),
+        callback = function()
+          vim.lsp.buf.clear_references()
+        end,
+      })
+    end
+  end,
+})
 
 -- read :h vim.lsp.config for changing options of lsp servers 
