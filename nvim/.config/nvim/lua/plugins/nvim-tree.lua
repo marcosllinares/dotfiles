c26b0a51865b6326fb1008c4fea820dcc4f4d107
Marcos Llinares <alu0100972443@ull.edu.es>
2025-09-29 18:29:52 +0100

    Nvim-tree configurado

---
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = function(_, opts)
+      -- ancho del panel
+      opts.view = vim.tbl_deep_extend("force", opts.view or {}, {
+        width = 36,      -- aquí pones el número de columnas que quieras
+        side = "left",   -- o "right" si lo quieres en el otro lado
+      })
+      opts.git = vim.tbl_deep_extend("force", opts.git or {}, {
+        enable = true,
+        show_on_dirs = false,
+        show_on_open_dirs = false,
+      })
+      opts.modified = { enable = true, show_on_dirs = false, show_on_open_dirs = false }
+      opts.renderer = vim.tbl_deep_extend("force", opts.renderer or {}, {
+        highlight_git = "none",                 -- "none" | "icon" | "name" | "all" | true
+        highlight_modified = "name",            -- "none" | "icon" | "name" | "all" 
+        icons = {
+          git_placement = "right_align",        -- o "right_align" , "after"
+          modified_placement = "after",
+          glyphs = {
+            modified = "✱",
+            git = {
+              unstaged  = "",
+              staged    = "",
+              unmerged  = "",
+              renamed   = "",
+              untracked = "",
+              deleted   = "",
+              ignored   = "󰴲",
+            },
+          },
+        },
+      })
+      return opts
+    end,
+  },
+}
